71067f9e8c760d9a90611f61772b75d5a6522326
Vishal0899 <97458281+Vishal0899@users.noreply.github.com>
2022-05-23 16:51:43 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,54 @@
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import Button from "./components/Button";
+import CandidateCard from "./components/CandidateCard";
+
+export default function App() {
+  const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [page, setPage] = useState(1);
+  const [sOrder, setSOrder] = useState("ASC")
+
+  useEffect(() => {
+    fetchData({ page, sOrder });
+  }, [page,sOrder]);
+
+  const fetchData = async ({ page, sOrder }) => {
+    setLoading(true)
+    axios({
+      method: "GET",
+      url: "http://localhost:8080/candidates",
+      params: {
+        _page: page,
+        _limit: 5,
+        _sort : "salary",
+        _order : sOrder,
+      },
+    })
+      .then((res) => {
+        setData(res.data);
+        setLoading(false);
+      })
+      .then((err) => {
+        console.log(err);
+        setLoading(false);
+      });
+  };
+
+
+  return (
+    <div className="App">
+      <div>
+        <div id="loading-container">
+          {loading == true ? "...Loading" : null}
+        </div>
+        <Button onClick={() => setSOrder(sOrder == "ASC" ? "DESC" : "ASC")} id="SORT_BUTTON" title={sOrder == "ASC" ? "Sort by Descending Salary" : "Sort by Ascending Salary"} />
+        <Button disabled={page == 1} onClick={() => setPage(page - 1)} title="PREV" id="PREV" />
+        <Button onClick={() => setPage(page + 1)} id="NEXT" title="NEXT" />
+      </div>
+      {data.map((item) => (
+        <CandidateCard key={item.id} {...item} />
+      ))}
+    </div>
+  );
+}
